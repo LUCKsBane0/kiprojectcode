c156d323be6a4daff7c89f1a6ac590a071a970a7
david <miner2.blablmgmail.co>
2022-01-14 00:37:10 +0100

    Logistregr

--- logisticregression.py
+++ logisticregression.py
@@ -15,6 +15,14 @@ triangles.drop(triangles.columns[0], axis=1, inplace=True)
 circles.drop(circles.columns[0], axis=1, inplace=True)
 squares.drop(squares.columns[0], axis=1, inplace=True)
 
+def split_data_set(df_to_be_split: pd.DataFrame):
+    holy_numbers = [i for i in range(0, len(df_to_be_split))]
+    random.shuffle(holy_numbers)
+    train_numbers = holy_numbers[:int(0.80*len(df_to_be_split))]
+    test_numbers = holy_numbers[int(0.80*len(df_to_be_split)):int(1.00*len(df_to_be_split))]
+    train_set = df_to_be_split.iloc[train_numbers]
+    test_set = df_to_be_split.iloc[test_numbers]
+    return train_set, test_set
 hearts['2500'] = 0
 triangles['2500'] = 1
 circles['2500'] = 2
